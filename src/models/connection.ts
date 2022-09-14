adcd89aa79accfbef676006ee1c861e672c31660
matheusmattos7 <matheusm.mattos7@gmail.com>
2022-09-14 14:56:03 -0300

    Iniciando O projeto

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import 'dotenv/config';
 
 const MONGO_DB_URL = 'mongodb://localhost:27017/CarShop';
-
+//
 const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGO_URI
     || MONGO_DB_URL,
