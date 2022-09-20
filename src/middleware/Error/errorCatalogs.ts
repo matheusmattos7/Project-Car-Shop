db4efcc293ffe59c44438e8adb0adcc66e34ccff
matheusmattos7 <matheusm.mattos7@gmail.com>
2022-09-20 17:42:04 -0300

    feat: Add errorCatalogs types

---
+++ src/middleware/Error/errorCatalogs.ts
@@ -0,0 +1,26 @@
+import StatusCode from 'http-status-codes';
+
+export enum ErrorTypes {
+  EntityNotFound = 'EntityNotFound',
+  InvalidMongoId = 'InvalidMongoId',
+}
+
+type ErrorResponseObject = {
+  message: string,
+  statusHttp: number,
+};
+
+export type ErrorCatalog = {
+  [key in ErrorTypes]: ErrorResponseObject
+};
+
+export const errorCatalog: ErrorCatalog = {
+  EntityNotFound: {
+    message: 'Entity not found',
+    statusHttp: StatusCode.NOT_FOUND,
+  },
+  InvalidMongoId: {
+    message: 'Id must be a 24 characters hexadecimal',
+    statusHttp: StatusCode.BAD_REQUEST,
+  },
+};
