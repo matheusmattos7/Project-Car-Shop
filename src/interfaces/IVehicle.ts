d50d0998e2b0ea8e626122ebba53a31adff1c556
matheusmattos7 <matheusm.mattos7@gmail.com>
2022-09-20 16:31:42 -0300

    feat: Create IVehicle interface

---
+++ src/interfaces/IVehicle.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+export const vehicleSchema = z.object({
+  model: z.string().min(3),
+  year: z.number().int().gte(1900).lte(2022),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number().int(),
+});
+
+export type IVehicle = z.infer<typeof vehicleSchema>;
