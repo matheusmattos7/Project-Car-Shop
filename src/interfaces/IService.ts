1c5d34fb06015b4348e909b1d25f1351c1285004
matheusmattos7 <matheusm.mattos7@gmail.com>
2022-09-21 01:35:12 -0300

    feat: add function readOne to CarsService

--- src/interfaces/IService.ts
+++ src/interfaces/IService.ts
@@ -1,7 +1,7 @@
 export interface IService<T> {
   create(obj:T):Promise<T>,
   read():Promise<T[]>,
-  // readOne(_id:string):Promise<T | null>,
+  readOne(_id:string):Promise<T | null>,
   // update(_id:string, obj: T):Promise<T | null>,
   // delete(_id:string):Promise<T | null>
 }
--- src/services/CarsService.ts
+++ src/services/CarsService.ts
@@ -1,6 +1,7 @@
 import { ICar, zodCarSchema } from '../interfaces/ICar';
 import { IModel } from '../interfaces/IModel';
 import { IService } from '../interfaces/IService';
+import { ErrorTypes } from '../middleware/Error/errorCatalogs';
 
 class CarsService implements IService<ICar> {
   private _cars: IModel<ICar>;
@@ -21,6 +22,15 @@ class CarsService implements IService<ICar> {
 
   public async read(): Promise<ICar[]> {
     return this._cars.read();
+  }
+
+  public async readOne(id: string): Promise<ICar | null> {
+    const result = this._cars.readOne(id);
+
+    if (!result) {
+      throw Error(ErrorTypes.EntityNotFound);
+    }
+    return result;
   }
 }
 
